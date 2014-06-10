a1a029770c4354fe197df031380b8c9d21430477
romainf <romain.faugeroux@gmail.com>
2014-06-10 10:27:29 +0200

    update buybuild

--- ACM/buybuild.cpp
+++ ACM/buybuild.cpp
@@ -5,18 +5,17 @@
 
 using namespace std;
 
-class Edge {
+class Node {
 
 public:
-    int a, b, cost;
+    int idx, cost;
 
-    Edge(int _a, int _b, int _cost){
-        a = _a;
-        b= _b;
+    Node(int _idx, int _cost){
+        idx = _idx;
         cost = _cost;
     }
 
-    bool operator< (const Edge& e2) const{
+    bool operator< (const Node& e2) const{
         return cost>e2.cost;
     }
 };
@@ -27,6 +26,7 @@ int main() {
     int n_tc;
     scanf("%d", &n_tc);
     for (int tc = 0; tc < n_tc; ++tc) {
+
         if(tc) printf("\n");
 
         int n_city, n_subn;
@@ -74,29 +74,30 @@ int main() {
             }
         }
 
-        bool used[n_city];
+        //Initial EMST (without sub-networks)
+        bool used0[n_city];
         for (int i = 0; i < n_city; ++i) {
-            used[i] = false;
+            used0[i] = false;
         }
-        priority_queue<Edge> toAdd;
-        used[0] = true;
+        priority_queue<Node> toAdd;
+        used0[0] = true;
         for (int i = 1; i < n_city; ++i) {
-            toAdd.push(Edge(0, i, d[0][i]));
+            toAdd.push(Node(i, d[0][i]));
         }
 
-        int n_city_used = 1;
+        int n_cities_used = 1;
         int mst_cost = 0;
-        while(n_city_used < n_city){
+        while(n_cities_used < n_city){
 
-            Edge e = toAdd.top();
+            Node e = toAdd.top();
             toAdd.pop();
-            if(used[e.b]) continue;
-            used[e.b] = true;
+            if(used0[e.idx]) continue;
+            used0[e.idx] = true;
             mst_cost += e.cost;
-            ++n_city_used;
+            ++n_cities_used;
             for (int i = 0; i < n_city; ++i) {
-                if(i==e.b) continue;
-                toAdd.push(Edge(e.b, i, d[e.b][i]));
+                if(i==e.idx || used0[i]) continue;
+                toAdd.push(Node(i, d[e.idx][i]));
             }
         }
 
@@ -113,7 +114,7 @@ int main() {
                 for (int i = 0; i < n_city; ++i) {
                     used[i] = false;
                 }
-                priority_queue<Edge> toAdd;
+                priority_queue<Node> toAdd;
                 used[0] = true;
                 for (int i = 1; i < n_city; ++i) {
                     int dist0 = d[0][i];
@@ -124,7 +125,7 @@ int main() {
                         }
                     }
                     if(subn[net_left[sub_i]][0] && subn[net_left[0]][i]) dist0 = 0;
-                    toAdd.push(Edge(0, i, dist0));
+                    toAdd.push(Node(i, dist0));
                 }
 
                 int n_city_used = 1;
@@ -134,23 +135,23 @@ int main() {
                 }
                 while(n_city_used < n_city){
 
-                    Edge e = toAdd.top();
+                    Node e = toAdd.top();
                     toAdd.pop();
-                    if(used[e.b]) continue;
-                    used[e.b] = true;
+                    if(used[e.idx]) continue;
+                    used[e.idx] = true;
                     temp_cost += e.cost;
                     ++n_city_used;
                     for (int i = 0; i < n_city; ++i) {
-                        if(i==e.b) continue;
-                        int distance = d[e.b][i];
+                        if(i==e.idx || used[i]) continue;
+                        int distance = d[e.idx][i];
                         for (unsigned int net_i = 0; net_i < selected_networks.size(); ++net_i) {
-                            if(subn[selected_networks[net_i]][e.b] && subn[selected_networks[net_i]][i]) {
+                            if(subn[selected_networks[net_i]][e.idx] && subn[selected_networks[net_i]][i]) {
                                 distance = 0;
                                 break;
                             }
                         }
-                        if(subn[net_left[sub_i]][e.b] && subn[net_left[sub_i]][i]) distance = 0;
-                        toAdd.push(Edge(e.b, i, distance));
+                        if(subn[net_left[sub_i]][e.idx] && subn[net_left[sub_i]][i]) distance = 0;
+                        toAdd.push(Node(i, distance));
                     }
                 }
                 if(temp_cost<mst_cost){
