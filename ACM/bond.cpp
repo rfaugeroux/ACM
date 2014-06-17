73b928c2b29f8a36914a35c507421780297aeeed
rfaugeroux <romain.faugeroux@gmail.com>
2014-06-17 17:13:11 +0200

    a

--- ACM/bond.cpp
+++ ACM/bond.cpp
@@ -7,24 +7,44 @@
 #include <map>
 #include <set>
 
-#define max(a, b) ((a)>(b)?(a):(b))
-#define min(a, b) ((a)<(b)?(a):(b))
+#define MAX(a, b) ((a)>(b)?(a):(b))
+#define MIN(a, b) ((a)<(b)?(a):(b))
 
 using namespace std;
 
-class Node {
-public:
-    int idx, dist;
 
-    Node(int i, int c){
-        idx = i;
-        dist = c;
+class Edge {
+public:
+    int a, b, cost;
+
+    Edge(int i, int j, int c){
+        a = i;
+        b = j;
+        cost = c;
     }
 
-    bool operator< (const Node& n2) const {
-        return dist > n2.dist;
+    bool operator< (const Edge& e2) const {
+        return cost > e2.cost;
     }
 };
+
+int parent[50000];
+int cost[50000];
+
+int find_parent(int x) {
+
+    return (parent[x]==x)?x:
+                          parent[x] = find_parent(parent[x]);
+}
+
+int find_cost(int x) {
+    return (cost[x]==x)?x:
+                          parent[x] = find_parent(parent[x]);
+}
+
+void connect(int x, int y, int cost){
+    parent[find_parent(y)]=find_parent(x);
+}
 
 int main() {
 
@@ -39,66 +59,53 @@ int main() {
 
         scanf("%d", &nr);
 
-        vector<Node> adj[nc];
+        priority_queue<Edge> edges;
 
         for (int i = 0; i < nr; ++i) {
             int to, from, danger;
             scanf("%d", &to);
             scanf("%d", &from);
             scanf("%d", &danger);
-            to--;
-            from--;
-            adj[to].push_back(Node(from, danger));
-            adj[from].push_back(Node(to, danger));
+            edges.push(Edge(to-1, from-1, danger));
         }
 
         int np;
         scanf("%d", &np);
 
-        map<int, map<int, int> > dist;
+        int start[nc];
+        int end[nc];
+        for (int i = 0; i < nc; ++i) {
+            start[i]=-1;
+            end[i]=-1;
+        }
 
-        int start[np];
-        int end[np];
         for (int i = 0; i < np; ++i) {
             int st, en;
             scanf("%d", &st);
             scanf("%d", &en);
-            start[i] = st-1;
-            end[i] = en-1;
+            st--;
+            en--;
+            start[st] = en;
+            end[en] = st;
         }
 
-        for (int pi = 0; pi < np; ++pi) {
-            int si = start[pi];
-            int ei = end[pi];
-
-            set<int> visited;
-            priority_queue<Node> toVisit;
-            toVisit.push(Node(si, 0));
-
-            while(!toVisit.empty()){
-                Node node = toVisit.top();
-                toVisit.pop();
-                if(visited.count(node.idx)) continue;
-                visited.insert(node.idx);
-
-                dist[si][node.idx] = node.dist;
-                dist[node.idx][si] = node.dist;
-                if(node.idx==ei) {
-                    break;
-                }
-                if(dist.count(node.idx) && dist[node.idx].count(ei)){
-                    dist[si][ei] = max(dist[node.idx][ei], dist[si][node.idx]);
-                    dist[ei][si] = dist[si][ei];
-                    break;
-                }
-                for (unsigned int vi = 0; vi < adj[node.idx].size(); ++vi) {
-                    Node v = adj[node.idx][vi];
-                    if(visited.count(v.idx)) continue;
-                    toVisit.push(Node(v.idx, max(node.dist,v.dist)));
-                }
-            }
-            printf("%d\n", dist[si][ei]);
+        int result[np];
+        for (int i = 0; i < np; ++i) {
+            result[i]=-1;
         }
+
+        for (int i = 0; i < nc; ++i) {
+            parent[i]=i;
+        }
+
+        while(edges.empty()){
+
+            Edge e = edges.top();
+            edges.pop();
+
+
+        }
+        printf("%d\n", dist[si][ei]);
     }
     return 0;
 }
